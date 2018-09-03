123897817f020c0d2e5c227e265da9873192f55c
YanPanMichael <yanpancsu@icloud.com>
2018-09-04 01:37:18 +0800

    change footer

--- src/components/PersonalFooter.js
+++ src/components/PersonalFooter.js
@@ -6,7 +6,7 @@ class PersonalFooter extends Component {
       <footer className="footer-area section-gap">
         <div className="container">
           <div className="row">
-            <div className="col-lg-5 col-md-6 col-sm-6">
+            <div className="col-lg-8 col-md-6 col-sm-6">
               <div className="single-footer-widget">
                 <h4>About Me</h4>
                 <p>We have tested a number of registry fix and clean utilities and present our top 3 list on our site for your convenience.</p>
@@ -15,23 +15,9 @@ class PersonalFooter extends Component {
                   <i className="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a></p>
               </div>
             </div>
-            <div className="col-lg-5 col-md-6 col-sm-6">
+            <div className="col-lg-2 col-md-6 col-sm-6">
               <div className="single-footer-widget">
-                <h4>Newsletter</h4>
-                <p>Stay updated with our latest trends</p>
-                <div className="" id="mc_embed_signup">
-                  <form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get">
-                    <div className="input-group">
-                      <input type="text" className="form-control" name="EMAIL" placeholder="Enter Email Address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Email Address '" required="" type="email" />
-                      <div className="input-group-btn">
-                        <button className="btn btn-default" type="submit">
-                          <span className="lnr lnr-arrow-right"></span>
-                        </button>
-                      </div>
-                      <div className="info"></div>
-                    </div>
-                  </form>
-                </div>
+                <h4></h4>
               </div>
             </div>
             <div className="col-lg-2 col-md-6 col-sm-6 social-widget">
@@ -39,10 +25,9 @@ class PersonalFooter extends Component {
                 <h4>Follow Me</h4>
                 <p>Let us be social</p>
                 <div className="footer-social d-flex align-items-center">
-                  <a href="#"><i className="fa fa-facebook"></i></a>
-                  <a href="#"><i className="fa fa-twitter"></i></a>
-                  <a href="#"><i className="fa fa-dribbble"></i></a>
-                  <a href="#"><i className="fa fa-behance"></i></a>
+                  <a href={"mailto:panyancsu@163.com?subject=test&cc=yanpancsu@icloud.com&body=use mailto sample"+' Copyright ' + new Date().getFullYear()}><i className="fa fa-envelope"></i></a>
+                  <a href="#"><i className="fa fa-qq"></i></a>
+                  <a href="#"><i className="fa fa-weixin"></i></a>
                 </div>
               </div>
             </div>
