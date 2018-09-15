13421a7756c72954712d93b7f1acf11ff48785ec
YanPanMichael <yanpancsu@icloud.com>
2018-09-16 01:32:39 +0800

    improve fade out and fade in style and add hover style

--- src/components/PersonalHomeaboutPersonInformation.js
+++ src/components/PersonalHomeaboutPersonInformation.js
@@ -29,25 +29,27 @@ class PersonalHomeaboutPersonInformation extends Component {
   }
 
   renderDifferentPart() {
-    return <div>
+    return <div className="left-part-container">
       <img className={`img-fluid ${!this.props.personalDetailFlag?'fadeIn':'fadeOut'}`} src={require('../assets/img/personal/about-img.png')} alt="" />
-      <div className={`person_information ${this.props.personalDetailFlag?'fadeIn':'fadeOut'}`}>
-        <ul>
-          {Object.keys(this.personalDetailData).map(elemKey => (
-            <li key={elemKey}><a href="#">{elemKey}</a></li>
-          ))}
-        </ul>
-        <ul>
-          {Object.keys(this.personalDetailData).map(elemKey => (
-            <li key={elemKey}><a href="#">{this.personalDetailData[elemKey]}</a></li>
+      <div className={`detail-absolute ${this.props.personalDetailFlag?'fadeIn':'fadeOut'}`}>
+        <div className={'person_information'}>
+          <ul>
+            {Object.keys(this.personalDetailData).map(elemKey => (
+              <li key={elemKey}><a href="#">{elemKey}</a></li>
+            ))}
+          </ul>
+          <ul>
+            {Object.keys(this.personalDetailData).map(elemKey => (
+              <li key={elemKey}><a href="#">{this.personalDetailData[elemKey]}</a></li>
+            ))}
+          </ul>
+        </div>
+        <ul className={'social_icon'}>
+          {Object.keys(this.socialDetailData).map(elemKey => (
+            <li key={elemKey}><a href={this.socialDetailData[elemKey]} target="_blank"><i className={`fa fa-${elemKey}`}></i></a></li>
           ))}
         </ul>
       </div>
-      <ul className={`social_icon ${this.props.personalDetailFlag?'fadeIn':'fadeOut'}`}>
-        {Object.keys(this.socialDetailData).map(elemKey => (
-          <li key={elemKey}><a href={this.socialDetailData[elemKey]} target="_blank"><i className={`fa fa-${elemKey}`}></i></a></li>
-        ))}
-      </ul>
     </div>
   }
 
