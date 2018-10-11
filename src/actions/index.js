4736d191a2d06d66c954f2fa720bb8e59ea9d05d
YanPanMichael <yanpancsu@icloud.com>
2018-10-12 00:11:19 +0800

    add my original english pdf resume

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,27 +1,27 @@
 let nextTodoId = 0
 export const addTodo = text => ({
-    type: 'ADD_TODO',
-    id: nextTodoId++,
-    text
+  type: 'ADD_TODO',
+  id: nextTodoId++,
+  text
 })
 
 export const setVisibilityFilter = filter => ({
-    type: 'SET_VISIBILITY_FILTER',
-    filter
+  type: 'SET_VISIBILITY_FILTER',
+  filter
 })
 
 export const toggleTodo = id => ({
-    type: 'TOGGLE_TODO',
-    id
+  type: 'TOGGLE_TODO',
+  id
 })
 
 export const setHeaderShadowFlag = flag => ({
-    type: 'SET_HEADER_SHADOW',
-    flag
+  type: 'SET_HEADER_SHADOW',
+  flag
 })
 
 export const VisibilityFilters = {
-    SHOW_ALL: 'SHOW_ALL',
-    SHOW_COMPLETED: 'SHOW_COMPLETED',
-    SHOW_ACTIVE: 'SHOW_ACTIVE'
+  SHOW_ALL: 'SHOW_ALL',
+  SHOW_COMPLETED: 'SHOW_COMPLETED',
+  SHOW_ACTIVE: 'SHOW_ACTIVE'
 }
