133f7a38c0d6ea49d64acc4836e12fc1857b1ea4
YanPanMichael <yanpancsu@icloud.com>
2018-10-14 23:58:51 +0800

    save

--- src/components/PDF/ResumePDF.js
+++ src/components/PDF/ResumePDF.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Document, Page } from 'react-pdf';
+import '../../css/resume-pdf.css';
 
 class ResumePDF extends Component {
   state = {
@@ -17,10 +18,17 @@ class ResumePDF extends Component {
     return (
       <div>
         <Document
+          className="pdf-resume-document"
           file={require('./panyan_english_version.pdf')}
           onLoadSuccess={this.onDocumentLoadSuccess}
+          loading={<div>Please wait!</div>}
+          onLoadError={(error) => alert('Error while loading pages! ' + error.message)}
+          noData="Please select a file."
         >
-          <Page pageNumber={pageNumber} />
+          <Page
+            pageNumber={pageNumber}
+            onLoadError={(error) => alert('Error while retreiving the outline! ' + error.message)}
+           />
         </Document>
         <p>Page {pageNumber} of {numPages}</p>
       </div>
