666862ef01036ed4044095f53dfb56bacde268f1
YanPanMichael <yanpancsu@icloud.com>
2018-10-17 00:12:54 +0800

    expand eslint

--- src/components/TodoList/Todo.js
+++ src/components/TodoList/Todo.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Todo extends Component {
-    render() {
-        let { onClick, completed, text } = this.props;
-        return (
-            <li
-                onClick={onClick}
-                style={{
-                    textDecoration: completed ? 'line-through' : 'none'
-                }}
-            >
-                {text}
-            </li>
-        )
-    }
+  render() {
+    const { onClick, completed, text } = this.props;
+    return (
+      <li
+        onClick={onClick}
+        style={{
+          textDecoration: completed ? 'line-through' : 'none'
+        }}
+      >
+        {text}
+      </li>
+    );
+  }
 }
 
 Todo.propTypes = {
-    onClick: PropTypes.func.isRequired,
-    completed: PropTypes.bool.isRequired,
-    text: PropTypes.string.isRequired
-}
+  onClick: PropTypes.func.isRequired,
+  completed: PropTypes.bool.isRequired,
+  text: PropTypes.string.isRequired,
+};
 
-export default Todo
+export default Todo;
