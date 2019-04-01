8c45d094931825d1efe82c15d14475dab740d78b
YanPanMichael <yanpancsu@icloud.com>
2019-04-02 00:35:37 +0800

    add React.creaeteElement(Element, {...args})

---
+++ src/common/WithActionPanel.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Elements from '../components/Elements/Elements';
+
+const WithActionPanel = ({ ...args }) => React.createElement(Elements, {...args});
+
+export default WithActionPanel;
